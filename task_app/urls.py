3cad10b28a41b56c0b59de0f2be57b35b8525416
Andrii Kravchenko <64310996+4ndyua@users.noreply.github.com>
2022-07-11 14:18:46 +0300

    interim commit

--- task_app/urls.py
+++ task_app/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 
-from task_app.views import TaskListView, TaskCreateView
+from task_app.views import TaskListView, TaskCreateView, \
+    TaskUpdateView, TaskDeleteView, task_change_complete, \
+    TagListView, TagCreateView, TagUpdateView, TagDeleteView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name='task-list'),
     path("create/", TaskCreateView.as_view(), name="task-create"),
-
+    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("<int:pk>/change_complete/",
+         task_change_complete,
+         name="task_change_complete"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = 'task_app'
